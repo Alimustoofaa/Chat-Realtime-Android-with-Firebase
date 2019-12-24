09de3c5a970029b70f1bec2153b3a67be8537c90
Alimustoofaa <56061857+Alimustoofaa@users.noreply.github.com>
2019-12-24 10:52:39 +0700

    test

--- app/src/main/java/com/alimustofa/chating/MainActivity.java
+++ app/src/main/java/com/alimustofa/chating/MainActivity.java
@@ -4,14 +4,36 @@ import androidx.appcompat.app.AppCompatActivity;
 import de.hdodenhof.circleimageview.CircleImageView;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.auth.FirebaseAuth;
+
+
+import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
     CircleImageView profile_image;
+    TextView username;
+
+    FirebaseUser firebaseUser;
+    DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getActionBar().setTitle("");
+
+        profile_image= findViewById(R.id.profile_image);
+        username=findViewById(R.id.username);
+
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        reference = FirebaseDatabase.getInstance().getReference ("Users").child(firebaseUser.getUid());
     }
 }
