8132fc8e8e87d323199a64c721f5021f13d0529c
Alimustoofaa <56061857+Alimustoofaa@users.noreply.github.com>
2019-12-24 10:23:36 +0700

    add circle image view

--- app/src/main/java/com/alimustofa/chating/MainActivity.java
+++ app/src/main/java/com/alimustofa/chating/MainActivity.java
@@ -1,10 +1,13 @@
 package com.alimustofa.chating;
 
 import androidx.appcompat.app.AppCompatActivity;
+import de.hdodenhof.circleimageview.CircleImageView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    CircleImageView profile_image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
